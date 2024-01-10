23b28339283d1bcef380eef69fd95410b4348f37
Alexander Drummond <113899680+alexanderdrummond@users.noreply.github.com>
2024-01-10 11:09:04 +0000

    Generate + save done

--- src/components/Palette/Test.tsx
+++ src/components/Palette/Test.tsx
@@ -1,7 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const ColorPalette: React.FC = () => {
-  const [colors, setColors] = useState<number[][]>([]);
+  const [currentColors, setCurrentColors] = useState<number[][]>([]);
+  const [savedPalettes, setSavedPalettes] = useState<number[][][]>(() => {
+    const localData = localStorage.getItem("palettes");
+    return localData ? JSON.parse(localData) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("palettes", JSON.stringify(savedPalettes));
+  }, [savedPalettes]);
 
   const fetchColors = () => {
     const url = "http://colormind.io/api/";
@@ -14,19 +22,28 @@ const ColorPalette: React.FC = () => {
     http.onreadystatechange = () => {
       if (http.readyState === 4 && http.status === 200) {
         const palette = JSON.parse(http.responseText).result;
-        setColors(palette);
+        setCurrentColors(palette);
       }
     };
 
     http.open("POST", url, true);
     http.send(JSON.stringify(data));
   };
+  const savePalette = () => {
+    setSavedPalettes([...savedPalettes, currentColors]);
+  };
+
+  const removePalette = (index: number) => {
+    const newPalettes = savedPalettes.filter((_, idx) => idx !== index);
+    setSavedPalettes(newPalettes);
+  };
 
   return (
     <div>
       <button onClick={fetchColors}>Generate Palette</button>
+      <button onClick={savePalette}>Save Palette</button>
       <div style={{ display: "flex" }}>
-        {colors.map((color, index) => (
+        {currentColors.map((color, index) => (
           <div
             key={index}
             style={{
@@ -35,6 +52,25 @@ const ColorPalette: React.FC = () => {
               height: 100,
             }}
           />
+        ))}
+      </div>
+      <div>
+        {savedPalettes.map((palette, index) => (
+          <div key={index}>
+            <div style={{ display: "flex" }}>
+              {palette.map((color, idx) => (
+                <div
+                  key={idx}
+                  style={{
+                    backgroundColor: `rgb(${color.join(",")})`,
+                    width: 100,
+                    height: 100,
+                  }}
+                />
+              ))}
+            </div>
+            <button onClick={() => removePalette(index)}>Remove</button>
+          </div>
         ))}
       </div>
     </div>
