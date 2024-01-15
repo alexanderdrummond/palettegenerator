dae01570815791125831b27d740b5fb77c876894
Alexander Drummond <113899680+alexanderdrummond@users.noreply.github.com>
2024-01-15 07:51:54 +0000

    Routing setup

--- src/components/Navbar/Nav.tsx
+++ src/components/Navbar/Nav.tsx
@@ -1,13 +1,31 @@
-import style from "./nav.module.scss"
+import { useNavigate } from "react-router-dom";
+import style from "./nav.module.scss";
 
 export const NavComponent = () => {
-return(
+  const navigate = useNavigate();
+
+  const navigateTo = (path: string) => {
+    navigate(path);
+  };
+
+  return (
     <nav className={style.navContainer}>
-        <h2>Colorizer Pallete Generator</h2>
-        <div className={style.buttonContainer}>
-        <button className={style.navButtonStyle}>Random Pallete</button>
-        <button className={style.navButtonStyle}>My Palletes</button>
-        </div>
+      <h2>Colorizer Palette Generator</h2>
+      <div className={style.buttonContainer}>
+        <button
+          className={style.navButtonStyle}
+          onClick={() => navigateTo("/")}
+        >
+          Random Palette
+        </button>
+
+        <button
+          className={style.navButtonStyle}
+          onClick={() => navigateTo("/saved-palettes")}
+        >
+          My Palettes
+        </button>
+      </div>
     </nav>
-)
-}
+  );
+};
---
+++ src/pages/SavedPalettes.tsx
@@ -0,0 +1,9 @@
+function SavedPalettes() {
+  return (
+    <div>
+      <h2>Saved Palettes</h2>
+    </div>
+  );
+}
+
+export default SavedPalettes;
